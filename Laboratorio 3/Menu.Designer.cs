b2aac30b4dabd6349fec34bfbc8d24221a3ff626
aaromCreate <aarom.ortiz4@gmail.com>
2025-10-02 08:32:41 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio 3/Menu.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Laboratorio_3
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.MnuBanco = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuNumero = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuImporte = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuSesion = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MnuBanco,
+            this.MnuNumero,
+            this.MnuImporte,
+            this.MnuSesion});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 29);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // MnuBanco
+            // 
+            this.MnuBanco.Font = new System.Drawing.Font("MingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MnuBanco.Name = "MnuBanco";
+            this.MnuBanco.Size = new System.Drawing.Size(82, 25);
+            this.MnuBanco.Text = "BANCO";
+            this.MnuBanco.Click += new System.EventHandler(this.MnuBanco_Click);
+            // 
+            // MnuNumero
+            // 
+            this.MnuNumero.Font = new System.Drawing.Font("MingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MnuNumero.Name = "MnuNumero";
+            this.MnuNumero.Size = new System.Drawing.Size(202, 25);
+            this.MnuNumero.Text = "NUMEROS ENTEROS";
+            this.MnuNumero.Click += new System.EventHandler(this.MnuNumero_Click);
+            // 
+            // MnuImporte
+            // 
+            this.MnuImporte.Font = new System.Drawing.Font("MingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MnuImporte.Name = "MnuImporte";
+            this.MnuImporte.Size = new System.Drawing.Size(118, 25);
+            this.MnuImporte.Text = "IMPORTES";
+            this.MnuImporte.Click += new System.EventHandler(this.MnuImporte_Click);
+            // 
+            // MnuSesion
+            // 
+            this.MnuSesion.Font = new System.Drawing.Font("MingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MnuSesion.Name = "MnuSesion";
+            this.MnuSesion.Size = new System.Drawing.Size(214, 25);
+            this.MnuSesion.Text = "INICIO DE SESION";
+            this.MnuSesion.Click += new System.EventHandler(this.MnuSesion_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip2);
+            this.IsMdiContainer = true;
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem MnuBanco;
+        private System.Windows.Forms.ToolStripMenuItem MnuNumero;
+        private System.Windows.Forms.ToolStripMenuItem MnuImporte;
+        private System.Windows.Forms.ToolStripMenuItem MnuSesion;
+    }
+}
+
---
+++ Laboratorio 3/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_3
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        
+
+        
+
+        
+
+        
+
+
+
+        private void MnuBanco_Click(object sender, EventArgs e)
+        {
+            Problema1 problema1 = new Problema1();
+            problema1.MdiParent = this;
+            problema1.WindowState = FormWindowState.Maximized;
+            problema1.Show();
+        }
+
+        private void MnuNumero_Click(object sender, EventArgs e)
+        {
+            Problema2 problema2 = new Problema2();
+            problema2.MdiParent = this;
+            problema2.WindowState = FormWindowState.Maximized;
+            problema2.Show();
+        }
+
+        private void MnuImporte_Click(object sender, EventArgs e)
+        {
+            Problema3 problema3 = new Problema3();
+            problema3.MdiParent = this;
+            problema3.WindowState = FormWindowState.Maximized;
+            problema3.Show();
+        }
+
+        private void MnuSesion_Click(object sender, EventArgs e)
+        {
+            Problema4 problema4 = new Problema4();
+            problema4.MdiParent = this;
+            problema4.WindowState = FormWindowState.Maximized;
+            problema4.Show();
+        }
+    }
+}
