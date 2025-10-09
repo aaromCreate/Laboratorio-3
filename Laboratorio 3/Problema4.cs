5a6ad7bf5bc76e6529019c029e40b96e2c3f0b74
Ahmed Diaz <ayodivi@gmail.com>
2025-10-09 09:20:11 -0500

    Agregando funcionalidad de keyup al Problema 4

--- Laboratorio 3/Problema4.cs
+++ Laboratorio 3/Problema4.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Laboratorio_3
@@ -20,35 +16,35 @@ namespace Laboratorio_3
         private void button1_Click(object sender, EventArgs e)
         {
             bool resultado = Verficacion.ValidarTodo(ref txtNombre, ref txtApellido, ref txtCorreo, ref txtContraseña, ref erpErrorNom, ref erpErrorApe, ref erpErrorCorreo, ref erpError);
+
             if (resultado)
                 MessageBox.Show($"Bienvenido {txtNombre.Text} {txtApellido.Text}", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Information);
             else
                 MessageBox.Show("Por favor, corrija los errores en los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
         }
 
         private void txtNombre_Validating(object sender, CancelEventArgs e)
         {
-            if (!Verficacion.ValidarTexto(txtNombre, erpErrorNom, "El nombre es obligatorio"))
-                e.Cancel = true;
+            bool esValido = Verficacion.ValidarTexto(txtNombre, erpErrorNom, "El nombre es obligatorio");
+            e.Cancel = !esValido; 
         }
 
         private void txtApellido_Validating(object sender, CancelEventArgs e)
         {
-            if (!Verficacion.ValidarTexto(txtApellido, erpErrorApe, "El apellido es obligatorio"))
-                e.Cancel = true;
+            bool esValido = Verficacion.ValidarTexto(txtApellido, erpErrorApe, "El apellido es obligatorio");
+            e.Cancel = !esValido;
         }
 
         private void txtCorreo_Validating(object sender, CancelEventArgs e)
         {
-            if (!Verficacion.ValidarCorreo(txtCorreo, erpErrorCorreo))
-                e.Cancel = true;
+            bool esValido = Verficacion.ValidarCorreo(txtCorreo, erpErrorCorreo);
+            e.Cancel = !esValido;
         }
 
         private void txtContraseña_Validating(object sender, CancelEventArgs e)
         {
-            if (!Verficacion.ValidarContraseña(txtContraseña, erpError))
-                e.Cancel = true;
+            bool esValido = Verficacion.ValidarContraseña(txtContraseña, erpError);
+            e.Cancel = !esValido;
         }
 
         private void txtContraseña_KeyDown(object sender, KeyEventArgs e)
@@ -63,12 +59,11 @@ namespace Laboratorio_3
         {
             if (char.IsDigit(e.KeyChar))
             {
-                e.Handled = true; 
+                e.Handled = true;
             }
-            
             else if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && e.KeyChar != ' ')
             {
-                e.Handled = true; 
+                e.Handled = true;
             }
         }
 
@@ -83,7 +78,25 @@ namespace Laboratorio_3
                 e.Handled = true;
             }
         }
+
+        private void txtContraseña_KeyUp(object sender, KeyEventArgs e)
+        {
+            Verficacion.ValidarContraseña(txtContraseña, erpError); 
+        }
+
+        private void txtCorreo_KeyUp(object sender, KeyEventArgs e)
+        {
+            Verficacion.ValidarCorreo(txtCorreo, erpErrorCorreo);
+        }
+
+        private void txtNombre_KeyUp(object sender, KeyEventArgs e)
+        {
+            Verficacion.ValidarTexto(txtNombre, erpErrorNom, "El nombre es obligatorio"); 
+        }
+
+        private void txtApellido_KeyUp(object sender, KeyEventArgs e)
+        {
+            Verficacion.ValidarTexto(txtNombre, erpErrorNom, "El apellido es obligatorio"); 
+        }
     }
 }
-       
-    
